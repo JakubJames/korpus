bcca48ce7c9a2620eb8324d765117216f8087a8a
Jakub Szymański <mrkubuw2@gmail.com>
2024-08-17 11:39:01 +0200

    partly add placing unit

--- UI/button_unit.gd
+++ UI/button_unit.gd
@@ -1,11 +1,14 @@
 extends Button
 class_name ButtonUnit
 
-@export var n_units: int
+enum UnitTypes {SIMPLE, ARCHER, BIG}
+
+@export var number_of_units: int
+@export var unit_type: UnitTypes
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$RichTextLabel.text = "[color=red]" + str(number_of_units) + "[/color]"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- UI/in_game_ui.gd
+++ UI/in_game_ui.gd
@@ -5,19 +5,35 @@ signal start_battle
 @export var korpus: Group
 @export var enemies: Group
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	#connect start_battle signal to all units
+var hold_unit: Unit
+
+func _process(delta: float) -> void:
+	if hold_unit:	
+		hold_unit.global_position = get_viewport().get_camera_2d().get_global_mouse_position()
+
+func _on_button_pressed() -> void:
+	# connect start_battle signal to all units
 	for unit in korpus.units_list:
+		print(unit.name)
 		connect("start_battle", unit._on_units_inventory_start_battle)
 	for unit in enemies.units_list:
 		connect("start_battle", unit._on_units_inventory_start_battle)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-func _on_button_pressed() -> void:
+	
 	start_battle.emit()
+	$UnitsInventoryContainer.hide()
+	$Button.text = 'STOP BATTLE'
+
+
+func _on_simple_unit_button_pressed() -> void:
+	hold_unit = preload("res://Characters/unit.tscn").instantiate()
+	korpus.add_child(hold_unit)
+	hold_unit.get_node("CollisionShape2D").disabled = true
+	hold_unit.target_group = enemies
+
+
+func _on_archery_unit_button_pressed() -> void:
+	print("archery placeholder")
+
+
+func _on_big_unit_button_pressed() -> void:
+	print("big placeholder")
